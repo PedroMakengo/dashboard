b99cc3735b5d650213bac89f5d555ce21f703eb3
PedroMakengo <pedromakengomiguel@gmail.com>
2023-05-05 02:45:39 +0100

    Trabalhando

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,55 @@
+import { Box, Stack, Text, Link, Icon } from '@chakra-ui/react'
+
+import {
+  RiDashboardLine,
+  RiContactsLine,
+  RiInputMethodLine,
+  RiGitMergeLine,
+} from 'react-icons/ri'
+export function Sidebar() {
+  return (
+    <Box as="aside" w="64" mr="8">
+      <Stack spacing="12" align="flex-start">
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            GERAL
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" alignItems="center">
+              <Icon as={RiDashboardLine} />
+              <Text ml="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+            <Link display="flex" alignItems="center">
+              <Icon as={RiContactsLine} />
+              <Text ml="4" fontWeight="medium">
+                Usuários
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            Automação
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" alignItems="center">
+              <Icon as={RiInputMethodLine} />
+              <Text ml="4" fontWeight="medium">
+                Formulário
+              </Text>
+            </Link>
+            <Link display="flex" alignItems="center">
+              <Icon as={RiGitMergeLine} />
+              <Text ml="4" fontWeight="medium">
+                Automação
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  )
+}
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,94 @@
+import { Flex, SimpleGrid, Box, Text, theme } from '@chakra-ui/react'
+import { Header } from '@/components/Header'
+import dynamic from 'next/dynamic'
+import { Sidebar } from '@/components/Sidebar'
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+})
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      '2021-01-18T00:00:00.00Z',
+      '2022-03-18T00:00:00.00Z',
+      '2021-03-18T00:00:00.00Z',
+      '2021-03-18T00:00:00.00Z',
+      '2021-03-18T00:00:00.00Z',
+      '2021-03-18T00:00:00.00Z',
+      '2021-03-18T00:00:00.00Z',
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.8,
+      opacityTo: 0.3,
+    },
+  },
+}
+const series = [{ name: 'series1', data: [31, 120, 120, 5, 30, 80] }]
+
+export default function Dashboard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1120} mx="auto" px="6">
+        <Sidebar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+              <Chart
+                options={options}
+                series={series}
+                type="area"
+                height={150}
+              />
+            </Text>
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Taxa de Abertura
+              <Chart
+                options={options}
+                series={series}
+                type="area"
+                height={150}
+              />
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  )
+}
